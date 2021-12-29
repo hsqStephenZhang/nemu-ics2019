f70ba9ca56753cce0e3547518462cad560ec7bca
tracer-ics2019 <tracer@njuics.org>
2021-12-29 12:40:33 +0800

    >  compile U201814795 zc Linux WIN-Q6NSV31D7NT 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:40:33 up 1 day, 12:18,  0 users,  load average: 0.00, 0.04, 0.18 632e5ee57d64785db71b21a5ae18763116671f5

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -28,57 +28,57 @@ int main(int argc, char *argv[])
 
 void debug_expression()
 {
-  // FILE *fp = fopen("/home/zc/ics2019/nemu/tools/gen-expr/input", "r");
-  // if (fp == NULL)
-  // {
-  //   printf("read file failed\n");
-  //   exit(-1);
-  // }
-  // else
-  // {
-  //   char *line = NULL;
-  //   size_t read;
-  //   size_t len;
-  //   int num_lines = 0;
-  //   printf("read file success\n");
-  //   while ((read = getline(line, &len, fp)) != -1)
-  //   {
-  //     printf("line:%s\n", line);
-  //     num_lines++;
-  //   }
-  //   printf("num lines:%d\n", num_lines);
-
-  //   if (line)
-  //   {
-  //     free(line);
-  //   }
-
-  //   fclose(fp);
-  // }
-
-#define NUM_LINES 100
-
-  FILE *fp = fopen("tools/gen-expr/input", "r");
-  char expression[70000];
-  char *str;
-  for (int i = 0; i < NUM_LINES; i++)
+  FILE *fp = fopen("/home/zc/ics2019/nemu/tools/gen-expr/input", "r");
+  if (fp == NULL)
   {
-    char flag = 1;
-
-    expression[0] = '\0';
-    fgets(expression, 70000, fp);
-    unsigned answer = 0, ans;
-    str = strtok(expression, " ");
-    sscanf(expression, "%u", &answer);
-    str = expression + strlen(expression) + 1;
-    printf("str:%s\n",str);
-    ans = expr(str, &flag);
-    if (answer != ans)
-      printf("%d Wrong\n", i);
-    else
-      printf("correct %d\n", i);
-    
+    printf("read file failed\n");
+    exit(-1);
   }
+  else
+  {
+    char *line = NULL;
+    size_t read;
+    size_t len;
+    int num_lines = 0;
+    printf("read file success\n");
+    while ((read = getline(line, &len, fp)) != -1)
+    {
+      printf("line:%s\n", line);
+      num_lines++;
+    }
+    printf("num lines:%d\n", num_lines);
+
+    if (line)
+    {
+      free(line);
+    }
+
+    fclose(fp);
+  }
+
+// #define NUM_LINES 100
+
+//   FILE *fp = fopen("tools/gen-expr/input", "r");
+//   char expression[70000];
+//   char *str;
+//   for (int i = 0; i < NUM_LINES; i++)
+//   {
+//     char flag = 1;
+
+//     expression[0] = '\0';
+//     fgets(expression, 70000, fp);
+//     unsigned answer = 0, ans;
+//     str = strtok(expression, " ");
+//     sscanf(expression, "%u", &answer);
+//     str = expression + strlen(expression) + 1;
+//     printf("str:%s\n",str);
+//     ans = expr(str, &flag);
+//     if (answer != ans)
+//       printf("%d Wrong\n", i);
+//     else
+//       printf("correct %d\n", i);
+    
+//   }
 }
 
 #endif // DEBUG
