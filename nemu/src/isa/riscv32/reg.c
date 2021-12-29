d80abc88b3689a0a880de708108c6a16823ba5d8
tracer-ics2019 <tracer@njuics.org>
2021-12-29 18:38:16 +0800

    >  compile U201814795 zc Linux WIN-Q6NSV31D7NT 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:38:16 up 1 day, 18:04,  0 users,  load average: 0.00, 0.19, 0.34 b3879dce23ccf8c5349d4573cc80249826790b7e

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -22,6 +22,7 @@ void isa_reg_display_reg(char *reg)
   printf("reg name: %s\n", reg);
   for (int i = 0; i < NUM_REGS; i++)
   {
+    printf("hello world!,%s %d", reg, reg_l(i));
     if (strcmp(reg, reg_l(i)) == 0)
     {
       // printf("%-3s = 0x%08x\n", regsl[i], reg_l(i));
