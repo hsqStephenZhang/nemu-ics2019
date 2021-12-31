7a6345fa7c3b0f3e3fb19139b217b0921bdf6e9e
tracer-ics2019 <tracer@njuics.org>
2021-12-31 11:19:06 +0800

    >  run U201814795 zc Linux WIN-Q6NSV31D7NT 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:19:06 up 2 days, 11:27,  0 users,  load average: 0.05, 0.07, 0.09 e76ea69545f662173d911af7bdb86bb883ab0267

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,20 +3,150 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+static void putc(char * dst, char ch, int offset) {
+  if (dst) {
+    dst[offset] = ch;
+  } else {
+    _putc(ch);
+  }
+}
+
+static int int_width(long value, int base) {
+  int count = 1;
+  while (value / base) {
+    count++;
+    value /= base;
+  }
+  return count;
+}
+
+static int print_s(const char * data, char * dst) {
+  const char * count = data;
+  while (*count) {
+    putc(dst, *count, count - data);
+    count++;
+  }
+  return count - data;
+}
+
+static char int_to_ch(int value) {
+  if (value < 10) {
+    return value + '0';
+  } else {
+    return value - 10 + 'a';
+  }
+}
+
+static int print_d(long d, int count, char * dst, int base) {
+  assert(base == 10 || base == 16);
+  if (d < 0) {
+    putc(dst, '-', count);
+    return print_d(-d, count, dst ? dst+1 : dst, base) +1;
+  }
+  
+  if (d / base) {
+    count += print_d(d / base, count, dst, base);
+    putc(dst, int_to_ch(d % base), count);
+  } else {
+    putc(dst, int_to_ch(d), count);
+  }
+  return count + 1;
+}
+
+
+static int print_p(unsigned long p, int count, char * dst) {
+  unsigned int base = 16;
+  assert(p >= 0);
+  if (p / base) {
+    count += print_p(p / base, count, dst);
+    putc(dst, int_to_ch(p % base), count);
+  } else {
+    putc(dst, int_to_ch(p), count);
+  }
+  return count + 1;
+}
+
+
 int printf(const char *fmt, ...) {
-  return 0;
+  va_list ap;
+  va_start(ap, fmt);
+  int count = vsprintf(NULL, fmt, ap);
+  va_end(ap);
+  putc(NULL, '\0', count);
+  return count;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
+
+  char *s;
+  long d;
+  char ch;
+  int count = 0;
+  int width;
+  while((ch = *fmt++)) {
+    if (ch != '%') {
+      putc(out, ch, count);
+      count++;
+      continue;
+    }
+    switch (*fmt++)
+    {
+    case 's':
+      s = va_arg(ap, char *);
+      count += out ? print_s(s, out + count) : print_s(s, NULL);
+      break;
+    case 'd':
+      d = va_arg(ap, int);
+      count += out ? print_d(d, 0, out + count, 10) : print_d(d, 0, NULL, 10);
+      break;
+    case 'c':
+      ch = va_arg(ap, int);
+      putc(out, ch, count);
+      count++;
+      break;
+    case 'x':
+      d = va_arg(ap, int);
+      count += out ? print_p(d, 0, out + count) : print_p(d, 0, NULL);
+      break;
+    case 'p':
+      d = va_arg(ap, unsigned long);
+      putc(out, '0', count++);
+      putc(out, 'x', count++);
+      count += print_p(d, 0, out ? out + count : out);
+      break;
+    case '0':
+
+      width = *fmt++ - '0';
+      d = va_arg(ap, int);
+      // 直接向对应的位置填充width - actual_witdth个字符
+      for (int i = 0; i < width - int_width(d, *fmt == 'd' ? 10 : 16); i++) {
+        putc(out, '0', count + i);
+      }
+      assert(width == 8);
+      if (*fmt == 'd') {
+        out ? print_d(d, 0, out + count, 10) : print_d(d, 0, NULL, 10);
+        count += width;
+      } else if (*fmt == 'x') {
+        out ? print_d(d, 0, out + count, 16) : print_d(d, 0, NULL, 16);
+        count += width;
+      } else {
+        assert(0);
+      }
+      fmt++;
+    default:
+      assert(0);
+    }
+  }
+  return count;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
-}
-
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  return 0;
+  va_list ap;
+  va_start(ap, fmt);
+  int count = vsprintf(out, fmt, ap);
+  va_end(ap);
+  putc(out, '\0', count);
+  return count;
 }
 
 #endif
