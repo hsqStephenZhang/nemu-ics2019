3721fac58625aa245cbf8c3bff146216e3d76c72
hsqStephenZhang <2250015961@qq.com>
2021-12-31 10:39:16 +0800

    remove unused isa

--- nemu/src/isa/mips32/exec/all-instr.h
+++
@@ -1,9 +0,0 @@
-#include "cpu/exec.h"
-
-make_EHelper(lui);
-
-make_EHelper(ld);
-make_EHelper(st);
-
-make_EHelper(inv);
-make_EHelper(nemu_trap);
--- nemu/src/isa/mips32/exec/compute.c
+++
@@ -1,8 +0,0 @@
-#include "cpu/exec.h"
-
-make_EHelper(lui) {
-  rtl_shli(&s0, &id_src2->val, 16);
-  rtl_sr(id_dest->reg, &s0, 4);
-
-  print_asm_template3(lui);
-}
