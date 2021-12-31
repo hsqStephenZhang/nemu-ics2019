67d76639888c1726c2fd69468ab5e156b0b32d41
tracer-ics2019 <tracer@njuics.org>
2021-12-31 16:34:13 +0800

    >  compile U201814795 zc Linux WIN-Q6NSV31D7NT 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:34:13 up  1:57,  0 users,  load average: 1.42, 0.56, 0.22 c2854bfd3eb3e8f3106e467c5436e620b95caf

--- nexus-am/am/src/nemu-common/nemu-timer.c
+++ nexus-am/am/src/nemu-common/nemu-timer.c
@@ -3,7 +3,7 @@
 #include <nemu.h>
 
 size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
-  printf("read timer");
+  // printf("read timer\n");
   switch (reg) {
     case _DEVREG_TIMER_UPTIME: {
       _DEV_TIMER_UPTIME_t *uptime = (_DEV_TIMER_UPTIME_t *)buf;
