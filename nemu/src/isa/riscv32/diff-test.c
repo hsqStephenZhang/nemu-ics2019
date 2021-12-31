4648225bd2e96ab9e9664c86f30db5b7e3541723
tracer-ics2019 <tracer@njuics.org>
2021-12-31 10:22:31 +0800

    >  compile U201814795 zc Linux WIN-Q6NSV31D7NT 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:22:31 up 2 days, 10:31,  0 users,  load average: 0.92, 0.35, 0.19 861854f1f210fdb1a294e4b7e84186b88c0e1e

--- nemu/src/isa/riscv32/diff-test.c
+++ nemu/src/isa/riscv32/diff-test.c
@@ -4,6 +4,7 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for (int i = 0; i < 32; i++) {
     if (ref_r->gpr[i]._32 != reg_l(i)) {
+      Log("grp %d's not expected", i);
       return false;
     }
   }
