538770a7d9d06fe38b2fb7b2b2552e6037e049ba
tracer-ics2019 <tracer@njuics.org>
2022-01-03 09:52:15 +0800

    >  compile U201814795 zc Linux WIN-Q6NSV31D7NT 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  09:52:15 up  6:48,  0 users,  load average: 0.09, 0.07, 0.02 24b8fe4ec13e5f13b744a894d26046664bdceb8e

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -52,7 +52,7 @@ static inline void update_screen()
 
 static void vga_io_handler(uint32_t offset, int len, bool is_write)
 {
-  Log("vga_io_handler called");
+  // Log("vga_io_handler called");
   if (is_write)
     update_screen();
 }
