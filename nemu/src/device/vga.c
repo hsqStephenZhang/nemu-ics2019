16c6b19983e6f1b3940b1a26fa80c2caaf2f0678
hsqStephenZhang <2250015961@qq.com>
2022-01-03 14:45:44 +0800

    fixing display problems

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -16,7 +16,7 @@
 #define SCREEN_PORT 0x100 // Note that this is not the standard
 #define SCREEN_MMIO 0xa1000100
 #define SYNC_PORT 0x104 // Note that this is not the standard
-#define SYNC_MMIO 0xa1000108
+#define SYNC_MMIO 0xa1000104
 #define SCREEN_H 300
 #define SCREEN_W 400
 
@@ -26,7 +26,6 @@ static SDL_Texture *texture = NULL;
 
 static uint32_t (*vmem)[SCREEN_W] = NULL;
 static uint32_t *screensize_port_base = NULL;
-static uint32_t *sync_port_base = NULL;
 
 static inline void test_vga()
 {
@@ -59,13 +58,6 @@ static void vga_io_handler(uint32_t offset, int len, bool is_write)
     update_screen();
 }
 
-static void vga_sync_handler(uint32_t offset, int len, bool is_write)
-{
-  Log("vga_sync_handler called");
-  if (is_write)
-    update_screen();
-}
-
 void init_vga()
 {
   char title[128];
@@ -88,17 +80,14 @@ void init_vga()
 
 #endif
 
+  // 32 bit height + width: read only
+  // 32 bit sync: read + write
   screensize_port_base = (void *)new_space(8);
   screensize_port_base[0] = ((SCREEN_W) << 16) | (SCREEN_H);
-  add_pio_map("screen", SCREEN_PORT, (void *)screensize_port_base, 4, vga_io_handler);
-  add_mmio_map("screen", SCREEN_MMIO, (void *)screensize_port_base, 4, vga_io_handler);
+  add_pio_map("screen", SCREEN_PORT, (void *)screensize_port_base, 8, vga_io_handler);
+  add_mmio_map("screen", SCREEN_MMIO, (void *)screensize_port_base, 8, vga_io_handler);
 
   vmem = (void *)new_space(0x80000);
   add_mmio_map("vmem", VMEM, (void *)vmem, 0x80000, NULL);
-
-  sync_port_base = (void *)new_space(4);
-  sync_port_base[0] = 0;
-  add_pio_map("sync", SYNC_PORT, (void *)sync_port_base, 4, vga_sync_handler);
-  add_mmio_map("sync", SYNC_MMIO, (void *)sync_port_base, 4, vga_sync_handler);
 }
 #endif /* HAS_IOE */
