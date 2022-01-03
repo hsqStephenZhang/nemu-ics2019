a5f8ca982eaaf5783c562f7a7d7d4e20870b9318
tracer-ics2019 <tracer@njuics.org>
2022-01-03 11:06:49 +0800

    >  compile U201814795 zc Linux WIN-Q6NSV31D7NT 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:06:49 up  8:02,  0 users,  load average: 0.00, 0.01, 0.05 f8cf012928a83c048d5326f8e47a1dc878dbf02e

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -75,7 +75,7 @@ void init_vga()
   SDL_CreateWindowAndRenderer(SCREEN_W * 2, SCREEN_H * 2, 0, &window, &renderer);
   SDL_SetWindowTitle(window, title);
 
-#define SDL_RENDER_TEST
+// #define SDL_RENDER_TEST
 
 #ifndef SDL_RENDER_TEST
 
