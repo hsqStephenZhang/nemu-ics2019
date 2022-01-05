248c4e66452f08f4780db80bcbade0c1e62a956f
tracer-ics2019 <tracer@njuics.org>
2022-01-05 16:09:32 +0800

    >  compile U201814795 zc Linux WIN-Q6NSV31D7NT 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:09:32 up  1:54,  0 users,  load average: 0.19, 0.23, 0.10 71e8cfcfcee75560aa85b5fa3b47692bd7cf4643

--- nemu/src/isa/riscv32/intr.c
+++ nemu/src/isa/riscv32/intr.c
@@ -1,6 +1,12 @@
 #include "rtl/rtl.h"
 
 void raise_intr(uint32_t NO, vaddr_t epc) {
+  /*
+  1. 将当前PC值保存到sepc寄存器
+  2. 在scause寄存器中设置异常号
+  3. 从stvec寄存器中取出异常入口地址
+  4. 跳转到异常入口地址
+  */
   csr_write(SEPC, epc);  
   csr_write(SCAUSE, NO); 
   interpret_rtl_j(csr_read(STVEC)); 
