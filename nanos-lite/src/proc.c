4c5034f0487ea5ec8ff2a9e8115f8ee1a3f89379
tracer-ics2019 <tracer@njuics.org>
2022-01-05 17:35:25 +0800

    >  run U201814795 zc Linux WIN-Q6NSV31D7NT 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:35:25 up  3:20,  0 users,  load average: 0.25, 0.21, 0.21 f311dc14121b2ae8feaa699b0a3fce81442d245

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,7 +27,7 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-  naive_uload(NULL, "/bin/dummy");
+  naive_uload(NULL, "/bin/text");
 }
 
 _Context* schedule(_Context *prev) {
