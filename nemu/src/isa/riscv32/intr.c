d204e7b40762593bb61ad37bcbe80848ac6f10c0
tracer-ics2019 <tracer@njuics.org>
2022-01-05 16:01:25 +0800

    >  compile U201814795 zc Linux WIN-Q6NSV31D7NT 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:01:25 up  1:46,  0 users,  load average: 0.13, 0.08, 0.02 b6a62e1dc679f4eb7f03044d8de7e3b6b71b578

--- nemu/src/isa/riscv32/intr.c
+++ nemu/src/isa/riscv32/intr.c
@@ -1,7 +1,6 @@
 #include "rtl/rtl.h"
 
 void raise_intr(uint32_t NO, vaddr_t epc) {
-  // Log("interrupt raised,no is: %d", NO);
   csr_write(SEPC, epc);  
   csr_write(SCAUSE, NO); 
   interpret_rtl_j(csr_read(STVEC)); 
