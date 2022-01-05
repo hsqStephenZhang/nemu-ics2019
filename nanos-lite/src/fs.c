a2750ea3b74c9fcb74c8df29ede622894823ce50
tracer-ics2019 <tracer@njuics.org>
2022-01-05 16:28:45 +0800

    >  run U201814795 zc Linux WIN-Q6NSV31D7NT 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:28:45 up  2:13,  0 users,  load average: 0.18, 0.20, 0.18 612dd2b6279d1855873ce28280ea3edcd19a9458

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,38 +1,148 @@
 #include "fs.h"
 
-typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
-typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
+extern size_t ramdisk_read(void*, size_t, size_t);
+extern size_t ramdisk_write(const void*, size_t, size_t);
 
-typedef struct {
+extern size_t events_read(void*, size_t, size_t);
+extern size_t dispinfo_read(void*, size_t, size_t);
+extern size_t serial_write(const void*, size_t, size_t);
+extern size_t fb_write(const void*, size_t, size_t);
+extern size_t fbsync_write(const void*, size_t, size_t);
+
+typedef size_t (*ReadFn)(void *buf, size_t offset, size_t len);
+typedef size_t (*WriteFn)(const void *buf, size_t offset, size_t len);
+
+typedef struct
+{
   char *name;
   size_t size;
   size_t disk_offset;
+  size_t open_offset;
   ReadFn read;
   WriteFn write;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
+enum
+{
+  FD_STDIN,
+  FD_STDOUT,
+  FD_STDERR,
+  FD_FB
+};
 
-size_t invalid_read(void *buf, size_t offset, size_t len) {
+size_t invalid_read(void *buf, size_t offset, size_t len)
+{
   panic("should not reach here");
   return 0;
 }
 
-size_t invalid_write(const void *buf, size_t offset, size_t len) {
+size_t invalid_write(const void *buf, size_t offset, size_t len)
+{
   panic("should not reach here");
   return 0;
 }
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  {"stdin", 0, 0, invalid_read, invalid_write},
-  {"stdout", 0, 0, invalid_read, invalid_write},
-  {"stderr", 0, 0, invalid_read, invalid_write},
+  {"stdin", 0, 0, 0, invalid_read, invalid_write},
+  {"stdout", 0, 0, 0, invalid_read, serial_write},
+  {"stderr", 0, 0, 0, invalid_read, serial_write},
+  {"/dev/events", 0xffffff, 0, 0, events_read, invalid_write},
+  {"/dev/tty", 0, 0, 0, invalid_read, serial_write},
+  {"/dev/fb", 0, 0, 0, invalid_read, fb_write},
+  {"/dev/fbsync", 0xffff, 0, 0, invalid_read, fbsync_write},
+  {"/proc/dispinfo", 128, 0, 0, dispinfo_read, invalid_write},
 #include "files.h"
 };
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
 
-void init_fs() {
-  // TODO: initialize the size of /dev/fb
+int fs_open(const char *pathname, int flags, int mode)
+{
+  // Log("尝试打开文件%s", pathname);
+  for (int i = 0; i < NR_FILES; i++)
+  {
+    if (!strcmp(pathname, file_table[i].name))
+    {
+      return i;
+    }
+  }
+  // assert(0 && "Can't find file");
+  assert(0);
+}
+
+size_t fs_read(int fd, void *buf, size_t len)
+{
+  if (fd >= 3 && (file_table[fd].open_offset + len >= file_table[fd].size))
+  {
+    // 如果要读的内容已经超过了文件剩余的内容，那么就要修改len
+    if (file_table[fd].size > file_table[fd].open_offset)
+      len = file_table[fd].size - file_table[fd].open_offset;
+    else
+      len = 0;
+  }
+  if (!file_table[fd].read)
+  {
+    // 说明是普通文件，那么就使用ramdisk_read操作
+    ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  }
+  else
+  {
+    // 如果是std相关输入输出流，会直接到invalid？
+    len = file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  }
+  file_table[fd].open_offset += len;
+  return len;
+}
+
+size_t fs_write(int fd, const void *buf, size_t len)
+{
+  // printf("fs_write = %s\n", (char *)buf);
+  if (fd >= 5 && (file_table[fd].open_offset + len > file_table[fd].size))
+  {
+    if (file_table[fd].size > file_table[fd].open_offset)
+      len = file_table[fd].size - file_table[fd].open_offset;
+    else
+      len = 0;
+  }
+  if (!file_table[fd].write)
+  {
+    // Log("文件编号fd为%d, write函数为NULL? %d", fd, file_table[fd].write == NULL);
+    ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  }
+  else
+  {
+    file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  }
+  file_table[fd].open_offset += len;
+  return len;
+}
+
+size_t fs_lseek(int fd, size_t offset, int whence)
+{
+  switch (whence)
+  {
+  case SEEK_SET:
+    file_table[fd].open_offset = offset;
+    break;
+  case SEEK_CUR:
+    file_table[fd].open_offset += offset;
+    break;
+  case SEEK_END:
+    file_table[fd].open_offset = file_table[fd].size + offset;
+    break;
+  }
+  return file_table[fd].open_offset;
+}
+
+int fs_close(int fd)
+{
+  file_table[fd].open_offset = 0;
+  return 0;
+}
+
+void init_fs()
+{
+  int fb = fs_open("/dev/fb", 0, 0);
+  file_table[fb].size = screen_width() * screen_height() * 4;
 }
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,12 +1,91 @@
 #include "common.h"
 #include "syscall.h"
 
-_Context* do_syscall(_Context *c) {
+int do_open(const char *path, int flags, int mode)
+{
+  TODO();
+  return 0;
+}
+
+int do_close(int fd)
+{
+  return 0;
+}
+
+int do_read(int fd, void *buf, size_t count)
+{
+  return 0;
+}
+
+int do_write(int fd, const void *buf, size_t count)
+{
+  return 0;
+}
+
+size_t do_lseek(int fd, size_t offset, int whence)
+{
+  return 0;
+}
+
+int do_brk(int addr)
+{
+  return 0;
+}
+
+_Context *do_syscall(_Context *c)
+{
   uintptr_t a[4];
   a[0] = c->GPR1;
 
-  switch (a[0]) {
-    default: panic("Unhandled syscall ID = %d", a[0]);
+  switch (a[0])
+  {
+  case SYS_yield:
+    _yield();
+    c->GPRx = 0;
+    break;
+  case SYS_open:
+    c->GPRx = fs_open((const char *)(a[1]), a[2], a[3]);
+    break;
+  case SYS_read:
+    c->GPRx = do_read(a[1], (void *)(a[2]), a[3]);
+    break;
+  case SYS_write:
+    c->GPRx = do_write(a[1], (void *)(a[2]), a[3]);
+    break;
+  case SYS_kill:
+    break;
+  case SYS_getpid:
+    break;
+  case SYS_close:
+    c->GPRx = do_close(a[1]);
+    break;
+  case SYS_lseek:
+    c->GPRx = do_lseek(a[1], a[2], a[3]);
+    break;
+  case SYS_brk:
+    c->GPRx = do_brk(a[1]);
+    break;
+  case SYS_fstat:
+    break;
+  case SYS_time:
+    break;
+  case SYS_signal:
+    break;
+  case SYS_execve:
+    printf("%s\n", a[1]);
+    naive_uload(NULL, (const char *)a[1]);
+    c->GPR2 = SYS_exit;
+    do_syscall(c);
+    break;
+  case SYS_fork:
+  case SYS_link:
+  case SYS_unlink:
+  case SYS_wait:
+  case SYS_times:
+  case SYS_gettimeofday:
+    break;
+  default:
+    panic("Unhandled syscall ID = %d", a[0]);
   }
 
   return NULL;
