9979fdd4d1d7eb86701a07dbcdc7e4c7d5584fa4
tracer-ics2019 <tracer@njuics.org>
2022-01-05 17:06:33 +0800

    >  run U201814795 zc Linux WIN-Q6NSV31D7NT 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:06:33 up  2:51,  0 users,  load average: 0.35, 0.30, 0.25 4e83ec722d9ad70b3bdd01f0a09a8f56498348a4

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -11,7 +11,26 @@
 #endif
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  return 0;
+  Log("loading");
+  Elf_Ehdr Ehdr;
+  int fd = fs_open(filename, 0, 0);
+  fs_lseek(fd, 0, SEEK_SET);
+  fs_read(fd, &Ehdr, sizeof(Ehdr));
+  for(int i = 0; i < Ehdr.e_phnum;i++){
+      Elf_Phdr Phdr;
+      fs_lseek(fd, Ehdr.e_phoff + i*Ehdr.e_phentsize, SEEK_SET);
+      fs_read(fd, &Phdr, sizeof(Phdr));
+      if(!(Phdr.p_type & PT_LOAD)){
+          continue;
+      }
+      fs_lseek(fd, Phdr.p_offset, SEEK_SET);
+      fs_read(fd, (void*)Phdr.p_vaddr, Phdr.p_filesz);
+      for(unsigned int i = Phdr.p_filesz; i < Phdr.p_memsz;i++){
+          ((char*)Phdr.p_vaddr)[i] = 0;
+      }
+  }
+
+  return Ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
