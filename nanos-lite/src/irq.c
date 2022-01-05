3d65010f9c5efa3dbc28b276f0c234cf6fc77061
tracer-ics2019 <tracer@njuics.org>
2022-01-05 14:53:47 +0800

    >  run U201814795 zc Linux WIN-Q6NSV31D7NT 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:53:47 up 38 min,  0 users,  load average: 0.08, 0.19, 0.16 6224efbf6c3b67e6b16e07d2dfcd53cdd2f79fcb

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,9 +1,12 @@
 #include "common.h"
 
 extern int _cte_init(_Context*(*handler)(_Event, _Context*));
+extern _Context* do_syscall(_Context *c);
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
+    case _EVENT_YIELD   : printf("_EVENT_YIELD \n");  break;
+    case _EVENT_SYSCALL : printf("_EVENT_SYSCALL \n"); do_syscall(c);         break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
